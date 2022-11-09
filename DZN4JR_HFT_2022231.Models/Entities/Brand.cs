54be85675ff13f40353d5acd4d10a8876d497d42
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-09 21:21:54 +0100

    paint entitie created

--- DZN4JR_HFT_2022231.Models/Entities/Brand.cs
+++ DZN4JR_HFT_2022231.Models/Entities/Brand.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DZN4JR_HFT_2022231.Models.Entities
 {
-    internal class Brand
+    [Table("Brands")]
+    public class Brand
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        
+        public virtual IEnumerable<Paint> Paints { get; set; }
+
+        public Brand()
+        {
+        }
+
+        public Brand(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}; Name: {Name}";
+        }
     }
 }
---
+++ DZN4JR_HFT_2022231.Models/Entities/Paint.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Models.Entities
+{
+    [Table("Paints")]
+    public class Paint
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        public int BasePrice { get; set; }
+
+        public int BrandId { get; set; }
+
+        [JsonIgnore]
+        public virtual Brand Brand { get; set; }
+
+        public Paint()
+        {
+        }
+
+        public Paint(int id, string type, int basePrice, int brandId)
+        {
+            Id = id;
+            Type = type;
+            BasePrice = basePrice;
+            BrandId = brandId;
+        }
+
+        public Paint(int id, string type, int basePrice, Brand brand)
+        {
+            Id = id;
+            Type = type;
+            BasePrice = basePrice;
+            BrandId = brand.Id;
+            Brand = brand;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}; Name: {Type}; BasePrice: {BasePrice}; BrandId: {BrandId}";
+        }
+    }
+}
