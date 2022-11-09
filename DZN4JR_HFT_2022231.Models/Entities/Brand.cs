ec970db67be54377ff538f7cdb16c331cd629514
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-09 21:01:02 +0100

    brand entitie created

---
+++ DZN4JR_HFT_2022231.Models/Entities/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Models.Entities
+{
+    internal class Brand
+    {
+    }
+}
