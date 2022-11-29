a98c7a4b29dc4fcc14acc621c77822631ddd48f1
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-29 18:17:15 +0100

    repository interface and class created

---
+++ DZN4JR_HFT_2022231.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Repository.Interfaces
+{
+    public interface IRepository<TEntity, TKey>
+    {
+        IQueryable<TEntity> ReadAll();
+
+        TEntity Read(TKey id);
+
+        TEntity Create(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
---
+++ DZN4JR_HFT_2022231.Repository/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using DZN4JR_HFT_2022231.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Repository.Repositories
+{
+    public abstract class BaseRepository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntity : class
+    {
+        protected DbContext Db;
+
+        protected BaseRepository(DbContext Db)
+        {
+            this.Db = Db;
+        }
+
+        public TEntity Create(TEntity entity)
+        {
+            var result = Db.Set<TEntity>().Add(entity);
+            Db.SaveChanges();
+
+            return result.Entity;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Db.Remove(entity);
+            Db.SaveChanges();
+        }
+
+        public abstract TEntity Read(TKey id);
+
+        public IQueryable<TEntity> ReadAll()
+        {
+            return Db.Set<TEntity>();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            var result = Db.Set<TEntity>().Update(entity);
+            Db.SaveChanges();
+
+            return result.Entity;
+        }
+    }
+}
