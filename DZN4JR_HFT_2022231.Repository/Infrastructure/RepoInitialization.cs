50f6212ac654ccd689a9884ceb7d5d7904718233
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-29 19:41:30 +0100

    repo initializer uploaded

---
+++ DZN4JR_HFT_2022231.Repository/Infrastructure/RepoInitialization.cs
@@ -0,0 +1,24 @@
+﻿using DZN4JR_HFT_2022231.Repository.DbContexts;
+using DZN4JR_HFT_2022231.Repository.Interfaces;
+using DZN4JR_HFT_2022231.Repository.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Repository.Infrastructure
+{
+    public static class RepoInitialization
+    {
+        public static void InitRepoServices(IServiceCollection services)
+        {
+            services.AddScoped<DbContext, PaintStoreMemoryDbContext>();
+            services.AddScoped<IPaintRepository, PaintRepository>();
+            services.AddScoped<IBrandRepository, BrandRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
