d604f82e748afaaece89778da18b225c939cefc9
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-29 19:41:56 +0100

    APIResult uploaded

---
+++ DZN4JR_HFT_2022231.Models/Model/APIResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Models.Model
+{
+    public class APIResult
+    {
+        public bool IsSuccess { get; set; }
+
+        public APIResult()
+        {
+        }
+
+        public APIResult(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+    }
+}
