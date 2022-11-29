51bb39c075f766c30ff2fa91243ebb1ca24f2c02
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-29 18:18:02 +0100

    customer repository and interface created

--- DZN4JR_HFT_2022231.Models/Entities/Customer.cs
+++ DZN4JR_HFT_2022231.Models/Entities/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -8,19 +10,22 @@ using System.Xml.Linq;
 
 namespace DZN4JR_HFT_2022231.Models.Entities
 {
+    [Table("Customers")]
     public class Customer
     {
-        public int CustomerId { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string CustomerName { get; set; }
-        public string Adresse { get; set; }
+        public string Adress { get; set; }
         public string Email { get; set; }
         public bool RegularCustomer { get; set; }
 
-        public Customer(int customerId, string customerName, string adresse, string email, bool regularCustomer)
+        public Customer(int customerId, string customerName, string adress, string email, bool regularCustomer)
         {
-            CustomerId = customerId;
+            Id = customerId;
             CustomerName = customerName;
-            Adresse = adresse;
+            Adress = adress;
             Email = email;
             RegularCustomer = regularCustomer;
         }
---
+++ DZN4JR_HFT_2022231.Repository/Repositories/PaintRepository.cs
@@ -0,0 +1,23 @@
+﻿using DZN4JR_HFT_2022231.Models.Entities;
+using DZN4JR_HFT_2022231.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Repository.Repositories
+{
+    public class PaintRepository : BaseRepository<Paint, int>, IPaintRepository
+    {
+        public PaintRepository(DbContext Db) : base(Db)
+        {
+        }
+
+        public override Paint Read(int id)
+        {
+            return ReadAll().SingleOrDefault(x => x.Id == id);
+        }
+    }
+}
