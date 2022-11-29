070dc7496994613a54a8ce217cf6463409c3063c
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-29 18:17:48 +0100

    brand repository and interface created

---
+++ DZN4JR_HFT_2022231.Repository/Repositories/BrandRepository.cs
@@ -0,0 +1,23 @@
+﻿using DZN4JR_HFT_2022231.Models.Entities;
+using DZN4JR_HFT_2022231.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Repository.Repositories
+{
+    public class BrandRepository : BaseRepository<Brand, int>, IBrandRepository
+    {
+        public BrandRepository(DbContext Db) : base(Db)
+        {
+        }
+
+        public override Brand Read(int id)
+        {
+            return ReadAll().SingleOrDefault(x => x.Id == id);
+        }
+    }
+}
