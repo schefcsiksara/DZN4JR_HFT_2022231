8aebf8249ff4acf673c3a5165f12dbd73ac95625
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-29 17:53:43 +0100

    Customer class created

---
+++ DZN4JR_HFT_2022231.Models/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DZN4JR_HFT_2022231.Models.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Adresse { get; set; }
+        public string Email { get; set; }
+        public bool RegularCustomer { get; set; }
+
+        public Customer(int customerId, string customerName, string adresse, string email, bool regularCustomer)
+        {
+            CustomerId = customerId;
+            CustomerName = customerName;
+            Adresse = adresse;
+            Email = email;
+            RegularCustomer = regularCustomer;
+        }
+       
+    }
+}
