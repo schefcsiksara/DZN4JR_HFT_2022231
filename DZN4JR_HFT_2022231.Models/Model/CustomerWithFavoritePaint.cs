86d5d531afae0251bddf042943645f5fd227eece
Schefcsik Sára <schefcsiksara@gmail.com>
2022-11-30 11:57:07 +0100

    CustomerWithFavoritePaint model created

---
+++ DZN4JR_HFT_2022231.Models/Model/CustomerWithFavoritePaint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZN4JR_HFT_2022231.Models.Model
+{
+    public class CustomerWithFavoritePaint
+    {
+        public string CustomerName { get; set; }
+        public string FavoritePaint { get; set; }
+
+        public CustomerWithFavoritePaint()
+        {
+
+        }
+
+        public CustomerWithFavoritePaint(string customerName, string favoritePaint)
+        {
+            CustomerName = customerName;
+            FavoritePaint = favoritePaint;
+        }
+
+        public override string ToString()
+        {
+            return $"{CustomerName} {FavoritePaint}";
+        }
+    }
+}
